495a59989108eb6ca152088fdc6ce1f3eca73366
zoecasado <zoecompu19@gmail.com>
2026-09-03 08:05:48 -0300

    Misión/Visión
    Empecé a diseñar esa página

---
+++ src/components/pages/MisionVision.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../style/components/layout/MisionVision.css";
+
+const MisionVision = (props) => {
+    return (
+        <div className = "misionVision">
+            <h1 className = "titulo">Misión, Visión y Valores</h1>
+            ::after
+            <p>Los principios que guían nuestro compromiso con la salud de la comunidad</p>
+        </div>
+    );
+}
+
+export default MisionVision; 
